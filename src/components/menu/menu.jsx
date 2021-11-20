3faa3ec61e73cf7422c934d07cd9dcf54ee3d0f5
Konstantin <you@example.com>
2021-11-20 12:28:35 +0300

    Make header and footer

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import {MENU_SETTINGS} from '../../const';
 
-const Menu = () => {
 
+const Menu = ({menuType}) => {
+
+  console.info(menuType)
   return (
-    <div>MENU</div>
+    <nav className={MENU_SETTINGS[menuType].className}>
+      <ul className={MENU_SETTINGS[menuType].listClassName}>
+        {MENU_SETTINGS[menuType].items.map((item) => (
+          <li className="menu__item" key={item.text}>
+            <Link to={item.link} className={MENU_SETTINGS[menuType].linkClassName}>{item.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
+Menu.propTypes = {
+  menuType: PropTypes.string.isRequired,
+};
+
 export default Menu;
+
--- src/const.js
+++ src/const.js
@@ -1,7 +1,43 @@
 export const AppRoute = {
   ROOT: `/`,
   CART: `/cart`,
+  ABOUT: `/about`,
+  SERVICE: `/service`,
+  PLACE: `/place`,
+  FAQ: `/faq`,
+  RETURN: `/return`,
+  BLOG: `/blog`,
   PAGE_NOT_FOUND: `/page-not-found`
 };
 
+export const MenuType = {
+  header: `header`,
+  footer: `footer`
+};
+
+export const MENU_SETTINGS = {
+  header: {
+    className: `header__menu menu`,
+    listClassName: `menu__list`,
+    linkClassName: `menu__link`,
+    items: [
+      {link: AppRoute.ROOT, text: `Каталог`},
+      {link: AppRoute.PLACE, text: `Где купить?`},
+      {link: AppRoute.ABOUT, text: `О компании`},
+      {link: AppRoute.SERVICE, text: `Cервис-центры`}
+    ]
+  },
+  footer: {
+    className: `footer__menu menu`,
+    listClassName: `menu__list menu__list--footer`,
+    linkClassName: `menu__link menu__link--footer`,
+    items: [
+      {link: AppRoute.PLACE, text: `Где купить?`},
+      {link: AppRoute.BLOG, text: `Блог`},
+      {link: AppRoute.FAQ, text: `Вопрос - ответ`},
+      {link: AppRoute.RETURN, text: `Возврат`},
+      {link: AppRoute.SERVICE, text: `Cервис-центры`}
+    ]
+  },
+};
 
